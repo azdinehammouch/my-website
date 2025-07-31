81b9b58f19121e71f46ed6583321774010660159
AZZEDDINE <azdinehammouch9@gmail.com>
2025-08-01 00:47:44 +0800

    add costume doamin name to vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-website/', // <-- Set this to your repo name
+  base: '/', // <-- Use '/' for custom domains!
   build: {
     assetsDir: 'assets',
     rollupOptions: {
